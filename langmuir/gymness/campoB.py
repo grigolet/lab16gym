affdeff953ccd328a8c2ad252da392c6678dfcea
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2017-03-01 16:32:25 +0100

    Fix path for magnetic field anaylsis

--- langmuir/gymness/campoB.py
+++ langmuir/gymness/campoB.py
@@ -2,7 +2,7 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 # read position z and B at 1000A
-x, y = np.genfromtxt('data/B_z_1000A.txt', unpack=True)
+x, y = np.genfromtxt('../langmuir/data/B_z_1000A.txt', unpack=True)
     
 x = (x + 0.64) * 100
 y = y * 1e4  # in gauss
